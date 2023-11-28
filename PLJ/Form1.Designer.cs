373efb8035dedb0e443e7797ec05540d44818193
kovacsg <gabor.kovacs@kuka.com>
2023-11-28 18:15:49 +0100

    update

--- PLJ/Form1.Designer.cs
+++ PLJ/Form1.Designer.cs
@@ -59,6 +59,7 @@
             p02 = new Panel();
             p01 = new Panel();
             p00 = new Panel();
+            button1 = new Button();
             groupBox1.SuspendLayout();
             panel11x.SuspendLayout();
             SuspendLayout();
@@ -90,81 +91,93 @@
             groupBox1.Controls.Add(p02);
             groupBox1.Controls.Add(p01);
             groupBox1.Controls.Add(p00);
-            groupBox1.Location = new Point(12, 12);
+            groupBox1.Location = new Point(10, 9);
+            groupBox1.Margin = new Padding(3, 2, 3, 2);
             groupBox1.Name = "groupBox1";
-            groupBox1.Size = new Size(286, 309);
+            groupBox1.Padding = new Padding(3, 2, 3, 2);
+            groupBox1.Size = new Size(250, 232);
             groupBox1.TabIndex = 0;
             groupBox1.TabStop = false;
             groupBox1.Text = "PLJ";
             // 
             // p44
             // 
-            p44.Location = new Point(230, 250);
+            p44.Location = new Point(201, 188);
+            p44.Margin = new Padding(3, 2, 3, 2);
             p44.Name = "p44";
-            p44.Size = new Size(50, 50);
+            p44.Size = new Size(44, 38);
             p44.TabIndex = 18;
             // 
             // p43
             // 
-            p43.Location = new Point(174, 250);
+            p43.Location = new Point(152, 188);
+            p43.Margin = new Padding(3, 2, 3, 2);
             p43.Name = "p43";
-            p43.Size = new Size(50, 50);
+            p43.Size = new Size(44, 38);
             p43.TabIndex = 17;
             // 
             // p42
             // 
-            p42.Location = new Point(118, 250);
+            p42.Location = new Point(103, 188);
+            p42.Margin = new Padding(3, 2, 3, 2);
             p42.Name = "p42";
-            p42.Size = new Size(50, 50);
+            p42.Size = new Size(44, 38);
             p42.TabIndex = 15;
             // 
             // p41
             // 
-            p41.Location = new Point(62, 250);
+            p41.Location = new Point(54, 188);
+            p41.Margin = new Padding(3, 2, 3, 2);
             p41.Name = "p41";
-            p41.Size = new Size(50, 50);
+            p41.Size = new Size(44, 38);
             p41.TabIndex = 16;
             // 
             // p40
             // 
-            p40.Location = new Point(6, 250);
+            p40.Location = new Point(5, 188);
+            p40.Margin = new Padding(3, 2, 3, 2);
             p40.Name = "p40";
-            p40.Size = new Size(50, 50);
+            p40.Size = new Size(44, 38);
             p40.TabIndex = 14;
             // 
             // p34
             // 
-            p34.Location = new Point(230, 194);
+            p34.Location = new Point(201, 146);
+            p34.Margin = new Padding(3, 2, 3, 2);
             p34.Name = "p34";
-            p34.Size = new Size(50, 50);
+            p34.Size = new Size(44, 38);
             p34.TabIndex = 13;
             // 
             // p33
             // 
-            p33.Location = new Point(174, 194);
+            p33.Location = new Point(152, 146);
+            p33.Margin = new Padding(3, 2, 3, 2);
             p33.Name = "p33";
-            p33.Size = new Size(50, 50);
+            p33.Size = new Size(44, 38);
             p33.TabIndex = 12;
             // 
             // p32
             // 
-            p32.Location = new Point(118, 194);
+            p32.Location = new Point(103, 146);
+            p32.Margin = new Padding(3, 2, 3, 2);
             p32.Name = "p32";
-            p32.Size = new Size(50, 50);
+            p32.Size = new Size(44, 38);
             p32.TabIndex = 10;
             // 
             // p31
             // 
-            p31.Location = new Point(62, 194);
+            p31.Location = new Point(54, 146);
+            p31.Margin = new Padding(3, 2, 3, 2);
             p31.Name = "p31";
-            p31.Size = new Size(50, 50);
+            p31.Size = new Size(44, 38);
             p31.TabIndex = 11;
             // 
             // p30
             // 
-            p30.Location = new Point(6, 194);
+            p30.Location = new Point(5, 146);
+            p30.Margin = new Padding(3, 2, 3, 2);
             p30.Name = "p30";
-            p30.Size = new Size(50, 50);
+            p30.Size = new Size(44, 38);
             p30.TabIndex = 9;
             // 
             // panel11x
@@ -174,150 +187,182 @@
             panel11x.Controls.Add(panel18);
             panel11x.Controls.Add(panel19);
             panel11x.Controls.Add(p24);
-            panel11x.Location = new Point(230, 138);
+            panel11x.Location = new Point(201, 104);
+            panel11x.Margin = new Padding(3, 2, 3, 2);
             panel11x.Name = "panel11x";
-            panel11x.Size = new Size(50, 50);
+            panel11x.Size = new Size(44, 38);
             panel11x.TabIndex = 8;
             // 
             // panel16
             // 
-            panel16.Location = new Point(224, 0);
+            panel16.Location = new Point(196, 0);
+            panel16.Margin = new Padding(3, 2, 3, 2);
             panel16.Name = "panel16";
-            panel16.Size = new Size(50, 50);
+            panel16.Size = new Size(44, 38);
             panel16.TabIndex = 8;
             // 
             // panel17
             // 
-            panel17.Location = new Point(168, 0);
+            panel17.Location = new Point(147, 0);
+            panel17.Margin = new Padding(3, 2, 3, 2);
             panel17.Name = "panel17";
-            panel17.Size = new Size(50, 50);
+            panel17.Size = new Size(44, 38);
             panel17.TabIndex = 7;
             // 
             // panel18
             // 
-            panel18.Location = new Point(112, 0);
+            panel18.Location = new Point(98, 0);
+            panel18.Margin = new Padding(3, 2, 3, 2);
             panel18.Name = "panel18";
-            panel18.Size = new Size(50, 50);
+            panel18.Size = new Size(44, 38);
             panel18.TabIndex = 5;
             // 
             // panel19
             // 
-            panel19.Location = new Point(56, 0);
+            panel19.Location = new Point(49, 0);
+            panel19.Margin = new Padding(3, 2, 3, 2);
             panel19.Name = "panel19";
-            panel19.Size = new Size(50, 50);
+            panel19.Size = new Size(44, 38);
             panel19.TabIndex = 6;
             // 
             // p24
             // 
             p24.Location = new Point(0, 0);
+            p24.Margin = new Padding(3, 2, 3, 2);
             p24.Name = "p24";
-            p24.Size = new Size(50, 50);
+            p24.Size = new Size(44, 38);
             p24.TabIndex = 4;
             // 
             // p23
             // 
-            p23.Location = new Point(174, 138);
+            p23.Location = new Point(152, 104);
+            p23.Margin = new Padding(3, 2, 3, 2);
             p23.Name = "p23";
-            p23.Size = new Size(50, 50);
+            p23.Size = new Size(44, 38);
             p23.TabIndex = 7;
             // 
             // p14
             // 
-            p14.Location = new Point(230, 82);
+            p14.Location = new Point(201, 62);
+            p14.Margin = new Padding(3, 2, 3, 2);
             p14.Name = "p14";
-            p14.Size = new Size(50, 50);
+            p14.Size = new Size(44, 38);
             p14.TabIndex = 8;
             // 
             // p22
             // 
-            p22.Location = new Point(118, 138);
+            p22.Location = new Point(103, 104);
+            p22.Margin = new Padding(3, 2, 3, 2);
             p22.Name = "p22";
-            p22.Size = new Size(50, 50);
+            p22.Size = new Size(44, 38);
             p22.TabIndex = 5;
             // 
             // p13
             // 
-            p13.Location = new Point(174, 82);
+            p13.Location = new Point(152, 62);
+            p13.Margin = new Padding(3, 2, 3, 2);
             p13.Name = "p13";
-            p13.Size = new Size(50, 50);
+            p13.Size = new Size(44, 38);
             p13.TabIndex = 7;
             // 
             // p21
             // 
-            p21.Location = new Point(62, 138);
+            p21.Location = new Point(54, 104);
+            p21.Margin = new Padding(3, 2, 3, 2);
             p21.Name = "p21";
-            p21.Size = new Size(50, 50);
+            p21.Size = new Size(44, 38);
             p21.TabIndex = 6;
             // 
             // p12
             // 
-            p12.Location = new Point(118, 82);
+            p12.Location = new Point(103, 62);
+            p12.Margin = new Padding(3, 2, 3, 2);
             p12.Name = "p12";
-            p12.Size = new Size(50, 50);
+            p12.Size = new Size(44, 38);
             p12.TabIndex = 5;
             // 
             // p20
             // 
-            p20.Location = new Point(6, 138);
+            p20.Location = new Point(5, 104);
+            p20.Margin = new Padding(3, 2, 3, 2);
             p20.Name = "p20";
-            p20.Size = new Size(50, 50);
+            p20.Size = new Size(44, 38);
             p20.TabIndex = 4;
             // 
             // p11
             // 
-            p11.Location = new Point(62, 82);
+            p11.Location = new Point(54, 62);
+            p11.Margin = new Padding(3, 2, 3, 2);
             p11.Name = "p11";
-            p11.Size = new Size(50, 50);
+            p11.Size = new Size(44, 38);
             p11.TabIndex = 6;
             // 
             // p10
             // 
-            p10.Location = new Point(6, 82);
+            p10.Location = new Point(5, 62);
+            p10.Margin = new Padding(3, 2, 3, 2);
             p10.Name = "p10";
-            p10.Size = new Size(50, 50);
+            p10.Size = new Size(44, 38);
             p10.TabIndex = 4;
             // 
             // p04
             // 
-            p04.Location = new Point(230, 26);
+            p04.Location = new Point(201, 20);
+            p04.Margin = new Padding(3, 2, 3, 2);
             p04.Name = "p04";
-            p04.Size = new Size(50, 50);
+            p04.Size = new Size(44, 38);
             p04.TabIndex = 3;
             // 
             // p03
             // 
-            p03.Location = new Point(174, 26);
+            p03.Location = new Point(152, 20);
+            p03.Margin = new Padding(3, 2, 3, 2);
             p03.Name = "p03";
-            p03.Size = new Size(50, 50);
+            p03.Size = new Size(44, 38);
             p03.TabIndex = 2;
             // 
             // p02
             // 
-            p02.Location = new Point(118, 26);
+            p02.Location = new Point(103, 20);
+            p02.Margin = new Padding(3, 2, 3, 2);
             p02.Name = "p02";
-            p02.Size = new Size(50, 50);
+            p02.Size = new Size(44, 38);
             p02.TabIndex = 1;
             // 
             // p01
             // 
-            p01.Location = new Point(62, 26);
+            p01.Location = new Point(54, 20);
+            p01.Margin = new Padding(3, 2, 3, 2);
             p01.Name = "p01";
-            p01.Size = new Size(50, 50);
+            p01.Size = new Size(44, 38);
             p01.TabIndex = 1;
             // 
             // p00
             // 
-            p00.Location = new Point(6, 26);
+            p00.Location = new Point(5, 20);
+            p00.Margin = new Padding(3, 2, 3, 2);
             p00.Name = "p00";
-            p00.Size = new Size(50, 50);
+            p00.Size = new Size(44, 38);
             p00.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(315, 40);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 1;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(700, 338);
+            Controls.Add(button1);
             Controls.Add(groupBox1);
+            Margin = new Padding(3, 2, 3, 2);
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
@@ -359,5 +404,6 @@
         private Panel p03;
         private Panel p02;
         private Panel p01;
+        private Button button1;
     }
 }
--- PLJ/Form1.cs
+++ PLJ/Form1.cs
@@ -7,9 +7,53 @@ namespace PLJ
             InitializeComponent();
         }
 
+        List<Panel> panels = new List<Panel>();
         private void Form1_Load(object sender, EventArgs e)
         {
+            panels.Add(p00);
+            panels.Add(p01);
+            panels.Add(p02);
+            panels.Add(p03);
+            panels.Add(p04);
 
+            panels.Add(p10);
+            panels.Add(p11);
+            panels.Add(p12);
+            panels.Add(p13);
+            panels.Add(p14);
+
+            panels.Add(p20);
+            panels.Add(p21);
+            panels.Add(p22);
+            panels.Add(p23);
+            panels.Add(p24);
+
+            panels.Add(p30);
+            panels.Add(p31);
+            panels.Add(p32);
+            panels.Add(p33);
+            panels.Add(p34);
+
+            panels.Add(p40);
+            panels.Add(p41);
+            panels.Add(p42);
+            panels.Add(p43);
+            panels.Add(p44);
+
+            foreach (var item in panels)
+            {
+                item.BackColor = Color.Black;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+          
+
+            foreach (var item in groupBox1.Controls)
+            {
+                ((Panel)item).BackColor = Color.Red;
+            }
         }
     }
 
