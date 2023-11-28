88e712f66de40ef8b99daf144d690946e4df0c87
balint.ivanics <balint.ivanics@kuka.com>
2023-11-28 17:49:57 +0100

    Create panels, color them

--- PLJ/Form1.Designer.cs
+++ PLJ/Form1.Designer.cs
@@ -20,6 +20,8 @@
             base.Dispose(disposing);
         }
 
+        List<List<Panel>> panelMatrix;
+
         #region Windows Form Designer generated code
 
         /// <summary>
@@ -32,8 +34,31 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Text = "Form1";
+            groupBox1 = new GroupBox();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Location = new Point(57, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(714, 377);
+            groupBox1.TabIndex = 2;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "groupBox1";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(groupBox1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
         }
 
         #endregion
+        private GroupBox groupBox1;
     }
 }
--- PLJ/Form1.cs
+++ PLJ/Form1.cs
@@ -5,6 +5,63 @@ namespace PLJ
         public Form1()
         {
             InitializeComponent();
+            setup();
+        }
+
+        private void setup()
+        {
+            int cells = 5;
+
+            int topCorner = 100;
+
+            int panelSize = 50;
+            int panelOffset = 10;
+
+            int groupSize = topCorner + cells * (panelSize + panelOffset);
+            groupBox1.Size = new Size(groupSize, groupSize);    
+
+            panelMatrix = new List<List<Panel>>();
+            for (int i = 0; i < cells; i++)
+            {
+                List<Panel> row = new List<Panel>();
+                for (int j = 0; j < cells; j++)
+                {
+                    Panel panel = new Panel();
+                    panel.Size = new Size(panelSize, panelSize);
+                    int xCoord = topCorner + i * (panelSize + panelOffset);
+                    int yCoord = topCorner + j * (panelSize + panelOffset);
+                    panel.Location = new Point(xCoord, yCoord);
+                    panel.Name = "panel" + i + ";" + j;
+                    panel.TabIndex = i + j;
+                    panel.BackColor = SystemColors.ActiveCaptionText;
+                    row.Add(panel);
+                    groupBox1.Controls.Add(panel);
+                }
+                panelMatrix.Add(row);
+            }
+            Button button = new Button();
+            button.Text = "Színezz!";
+            button.Click += buttonClick;
+            button.Location = new Point(panelSize, panelSize);
+            button.Size = new Size(panelSize, panelSize);
+            groupBox1.Controls.Add(button);
+            
+        }
+
+        private void buttonClick(object sender, EventArgs e)
+        {
+            foreach(List<Panel> panelList in panelMatrix)
+            {
+                foreach (Panel panel in panelList)
+                {
+                    panel.BackColor = Color.Red;
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
