66c1917e8322b75f22bbb598bd51da198d08684b
kovacsg <gabor.kovacs@kuka.com>
2024-01-30 16:15:26 +0100

    Add tile pictures to Resources

--- PLJ/Properties/Resources.Designer.cs
+++ PLJ/Properties/Resources.Designer.cs
@@ -59,5 +59,25 @@ namespace PLJ.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap black_tile_smashed {
+            get {
+                object obj = ResourceManager.GetObject("black_tile_smashed", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap red_tile_smashed {
+            get {
+                object obj = ResourceManager.GetObject("red_tile_smashed", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
